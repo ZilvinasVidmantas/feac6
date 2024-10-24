0302d3e08ddcf1ce5cb30071a0e2d9919b5eec64
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2024-10-24 19:32:03 +0300

    active class usege for NavLink

--- 3 - react-router-dom and custom hooks/src/routing/router.jsx
+++ 3 - react-router-dom and custom hooks/src/routing/router.jsx
@@ -2,22 +2,27 @@ import { HomePage } from '../pages/home-page';
 import { PostsPage } from '../pages/posts-page';
 import { AboutPage } from '../pages/about-page';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { NavbarLayout } from '../layouts/navbar-layout';
 
 // Outlet ir Layout
-const router = createBrowserRouter([
-  {
-    path: '/',
-    Component: HomePage
-  },
-  {
-    path: '/posts',
-    Component: PostsPage
-  },
-  {
-    path: '/about',
-    Component: AboutPage
-  }
-]);
+const router = createBrowserRouter([{
+  path: '/',
+  Component: NavbarLayout,
+  children: [
+    {
+      path: '/',
+      Component: HomePage
+    },
+    {
+      path: '/posts',
+      Component: PostsPage
+    },
+    {
+      path: '/about',
+      Component: AboutPage
+    }
+  ]
+}]);
 
 export const AppRouter = () => <RouterProvider router={router} />
 
